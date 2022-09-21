cb233a7deab40510e9266a4237a2290329b041ea
Ryle Edwards <ryle.edwards@gmail.com>
2022-09-20 21:20:40 -0500

    Move listeners.init() call up in code

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { Task } from "./modules/task";
 
 let projects = [];
 
+listeners.init();
+
 const testProject = new Project("Test Project");
 testProject.addTask(
   new Task("Test Task", "Test Description", "Test DueDate", "High")
 );
 
 dom.showTasks(testProject);
-
-listeners.init();
 
 const btnTest = document.createElement("button");
 btnTest.innerText = "TEST";
